efca0b9854126b1abb24aba39fc2c4f984316667
RishiRohanKalapala <156566826+RishiRohanKalapala@users.noreply.github.com>
2025-08-09 00:01:05 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -351,52 +351,52 @@ const Index = () => {
                   items={[
                     {
                       id: 1,
-                      name: "John Doe",
-                      designation: "Software Engineer",
+                      name: "Rishi Rohan Kalapala",
+                      designation: "Lead Developer",
                       image:
-                        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+                        "https://avatars.githubusercontent.com/u/7940844?s=80&v=4",
                     },
                     {
                       id: 2,
-                      name: "Robert Johnson",
-                      designation: "Product Manager",
+                      name: "Abhi Vignesh K",
+                      designation: "Web Sockets",
                       image:
-                        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+                        "https://github.com/identicons/mwhite.png",
                     },
                     {
                       id: 3,
-                      name: "Jane Smith",
-                      designation: "Data Scientist",
+                      name: "Shubhang G",
+                      designation: "UI Designer",
                       image:
-                        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+                        "https://avatars.githubusercontent.com/u/627410?v=4&size=80",
                     },
                     {
                       id: 4,
-                      name: "Emily Davis",
-                      designation: "UX Designer",
+                      name: "Naveen Nunna",
+                      designation: "Graphic Designer",
                       image:
-                        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+                        "https://avatars.githubusercontent.com/u/81637465?s=80&v=4",
                     },
                     {
                       id: 5,
-                      name: "Tyler Durden",
-                      designation: "Soap Developer",
+                      name: "Dhanush Reddy",
+                      designation: "Content Contributer",
                       image:
-                        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+                        "https://avatars.githubusercontent.com/u/7940844?s=80&v=4",
                     },
                     {
                       id: 6,
-                      name: "Dora",
-                      designation: "The Explorer",
+                      name: "Varshith R",
+                      designation: "Tester",
                       image:
-                        "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+                        "https://avatars.githubusercontent.com/u/108717211?s=80&v=4",
                     },
                     {
                       id: 7,
-                      name: "Alex Brown",
-                      designation: "Full‑stack Engineer",
+                      name: "Yuvraj A",
+                      designation: "Tester",
                       image:
-                        "https://images.unsplash.com/photo-1502767089025-6572583495b4?q=80&w=800&auto=format&fit=crop",
+                        "https://avatars.githubusercontent.com/u/58804827?s=64&v=4",
                     },
                   ]}
                 />
